4f8fde6a6ef283a4d4ac124a8d388f5850500d9c
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2023-05-26 19:57:39 +0530

    page2.php

---
+++ page2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page 2</title>
+</head>
+<body>
+    <h2>Page 2: Additional Information</h2>
+    <form method="POST" action="success.php">
+        <label for="location">Location:</label>
+        <input type="text" name="location" required><br><br>
+
+        <label for="age">Age:</label>
+        <input type="number" name="age" required><br><br>
+
+        <label for="university">University:</label>
+        <input type="text" name="university" required><br><br>
+
+        <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
