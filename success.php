00d42b20df6876ec8a485c0a8ce24c8781db725d
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2023-05-26 19:58:14 +0530

    success.php

---
+++ success.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        echo "<h2>Thank you for filling the form, $name!</h2>";
+    } else {
+        echo "<h2>Error: Invalid form submission.</h2>";
+    }
+    ?>
+</body>
+</html>
+
